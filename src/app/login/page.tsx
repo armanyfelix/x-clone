d357f58a5d233457ebdb64c8f86d5295a261e9b8
armanyfelix <luis.armany.felix@gmail.com>
2023-09-15 01:26:19 -0700

    colectivity

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,11 @@ import { AuthButtonServer } from '@/app/components/auth-button-server'
 
 export default function Login () {
   return (
-    <section className="grid place-content-center min-h-screen">
-      <h1 className="text-xl font-bold mb-4">Inicia sesión en DevTer</h1>
+    <section className="grid place-content-center text-center min-h-screen">
+      <h1 className="text-xl font-bold mb-4">
+        Inicia sesión en <span className="line-through">twitter</span>
+        <span> X-clone</span>
+      </h1>
       <AuthButtonServer />
     </section>
   )
